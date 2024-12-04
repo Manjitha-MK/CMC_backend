781af3ea3328e30178ee9b0af0563f90f8382d45
Manjitha-MK <manjithataridu123@gmail.com>
2024-12-04 22:47:05 +0530

    Create createProduct function 😎

--- controller/productController.js
+++ controller/productController.js
@@ -1,3 +1,27 @@
 import Product from "../models/products.js";
+import { isAdmin } from "./userController.js";
+
+export function createProduct(req, res) {
+
+    if(!isAdmin(req)){
+        res.jason({
+            message : "Please login as administrator to add products",
+        })
+    }
 
 
+
+  const product = new Product(newProductData);
+
+  Product.save()
+    .then(() => {
+      res.jason({
+        message: "Product created",
+      });
+    })
+    .catch(() => {
+      res.jason({
+        message: error,
+      });
+    });
+}
