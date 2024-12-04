6ffba23caa6f03949d110808d4afe8cf1f415085
Manjitha-MK <manjithataridu123@gmail.com>
2024-12-04 13:53:02 +0530

    Create products attributes 🫡✌️

--- models/products.js
+++ models/products.js
@@ -1,5 +1,39 @@
 import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema({
-    
+    productId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    productName : {
+        type : String,
+        required : true
+    },
+    altNames : [
+        {
+            type : String
+        }
+    ],
+    images : [
+        {
+            type : String
+        }
+    ],
+    price : {
+        type : Number,
+        required : true
+    },
+    lastPrice : {
+        type : Number,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    }
 });
