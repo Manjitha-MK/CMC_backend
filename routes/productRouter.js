ff2528d27ae29cb0c2ff5c8bc711525326d3b19d
Manjitha-MK <manjithataridu123@gmail.com>
2025-01-14 12:48:11 +0530

    create routes

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createProduct, deleteProduct, getProduct, updateProduct } from "../controller/productController.js";
+import { createProduct, deleteProduct, getProduct, getProductById, updateProduct } from "../controller/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct);
 productRouter.get("/",getProduct);
+productRouter.get("/:productId",getProductById)
 productRouter.delete("/:productId",deleteProduct);
 productRouter.put("/:productId",updateProduct);
 
