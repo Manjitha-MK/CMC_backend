8596bbff97dc7133100dc161562ae430c6f9aa61
Manjitha-MK <manjithataridu123@gmail.com>
2025-05-27 14:46:58 +0530

    😎🙂👍

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { createProduct, deleteProduct, getProduct, getProductById, updateProduct } from "../controller/productController.js";
+import { createProduct, deleteProduct, getProduct, getProductById, searchProducts, updateProduct } from "../controller/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct);
 productRouter.get("/",getProduct);
+productRouter.get("/search/:query",searchProducts)
 productRouter.get("/:productId",getProductById)
 productRouter.delete("/:productId",deleteProduct);
 productRouter.put("/:productId",updateProduct);
+
 
 export default productRouter;
