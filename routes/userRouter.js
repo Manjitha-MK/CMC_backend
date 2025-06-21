c33a55ef8ef1de62faa9dab60d76df8b0ddcbf28
Manjitha-MK <manjithataridu123@gmail.com>
2025-06-21 22:29:42 +0530

    add GET / route to fetch all users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createUser, googleLogin, loginUser } from "../controller/userController.js";
+import { createUser, getAllUsers, googleLogin, loginUser } from "../controller/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
 userRouter.post("/google",googleLogin)
+userRouter.get("/",getAllUsers)
 
 export default userRouter;
